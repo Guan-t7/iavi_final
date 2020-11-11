1fc0101e4f080918874c5a33eacdc2a1cc841d64
何苗 <736558434@qq.com>
2020-11-11 19:15:50 +0800

    改了相机间距和新的视差图计算函数

--- distDetect.py
+++ distDetect.py
@@ -16,7 +16,8 @@ def getXYZ(img1, img2, p):
     num = 4
     blockSize = 11
     # stereo = cv.StereoBM_create(numDisparities=16*num, blockSize=blockSize)
-    window_size = 3
+    '''
+    window_size = 18
     min_disp = 48
     num_disp = 112-min_disp
     stereo = cv.StereoSGBM_create(minDisparity = min_disp,
@@ -29,24 +30,63 @@ def getXYZ(img1, img2, p):
         speckleWindowSize = 100,
         speckleRange = 32
     )
+    '''
+    
+    opencv_measure_version = int(cv.__version__.split('.')[0])
+    windowSize = 7
+    minDisp = 10
+    numDisp = 250 - minDisp
+    
+    # for OpenCV3
+    stereo = cv.StereoSGBM_create(
+            minDisparity=minDisp,
+            numDisparities=numDisp,
+            blockSize=16,
+            P1=8*3*windowSize**2,
+            P2=32*3*windowSize**2,
+            disp12MaxDiff=1,
+            uniquenessRatio=10,
+            speckleWindowSize=100,
+            speckleRange=32
+        )
 
+
+    # calculate histogram
+    imtGrayL = cv.equalizeHist(imgL)
+    imtGrayR = cv.equalizeHist(imgR)
+    
+    # through gausiann filter
+    imgGrayL = cv.GaussianBlur(imtGrayL, (5, 5), 0)
+    imgGrayR = cv.GaussianBlur(imtGrayR, (5, 5), 0)
+    
+    # calculate disparity
+    disparity = stereo.compute(imgGrayL, imgGrayR).astype(np.float32)/16
+    disparity = (disparity - minDisp) / numDisp
+    disparity1 = cv.resize(disparity, None, fx=0.25, fy=0.25)
+    cv.imshow("disparity", disparity1)
+    
+    '''
     disparity = stereo.compute(imgL, imgR)
     disparity = cv.convertScaleAbs(disparity, alpha=255/disparity.max())
     disp_colored = cv.applyColorMap(disparity, cv.COLORMAP_JET)
     disp_colored = cv.resize(disp_colored, None, fx=0.25, fy=0.25)
     cv.imshow('disparity', disp_colored)
-    # disparity.convertTo(dispf, cv.CV_32F, 1.0/16.0)
-    dispf = np.float32(disparity)
-    dispf = dispf * 1.0/16.0
     
+    '''
+    # disparity.convertTo(dispf, cv.CV_32F, 1.0/16.0)
+    # dispf = np.float32(disparity)
+    # dispf = dispf * 1.0/16.0
+
     x = p[0]
     y = p[1]
-    z = dispf[p[1],p[0]]
+    z = disparity[p[1],p[0]]
     xyzw = np.array([[x],[y],[z],[1.0]])
     XYZW = np.matmul(cc.Q, xyzw)
     XYZ = XYZW[:3]/XYZW[3][0]
     
     return XYZ
+    
+    #return np.array([[1],[2],[3]])
 
 def getDistance(imgL, imgR, p1, p2):
     XYZ1 = getXYZ(imgL, imgR, p1)
